e55d52312294973dc91ad3782cce91b4a0cc87cf
bluevoxel <bluevox3l@gmail.com>
2016-03-21 17:38:55 +0100

    Minor changes.

--- src/org/contour2dplot/CubicInterpolator.java
+++ src/org/contour2dplot/CubicInterpolator.java
@@ -6,12 +6,16 @@ package org.contour2dplot;
 public class CubicInterpolator {
 
     public static double getValue(double[] p, double x) {
+
         int xi = (int) x;
+
         x -= xi;
+
         double p0 = p[Math.max(0, xi - 1)];
         double p1 = p[xi];
-        double p2 = p[Math.min(p.length - 1,xi + 1)];
+        double p2 = p[Math.min(p.length - 1, xi + 1)];
         double p3 = p[Math.min(p.length - 1, xi + 2)];
+
         return p1 + 0.5 * x * (p2 - p0 + x * (2.0 * p0 - 5.0 * p1 + 4.0 * p2 - p3 + x * (3.0 * (p1 - p2) + p3 - p0)));
     }
 }
